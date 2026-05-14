02c35d011dfd8a13c628523d1e1b5df1539c4080
Nileshsarwade <nsarwade50@gmail.com>
2026-05-15 00:32:10 +0530

    feat: add microphone recording and audio playback

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,60 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { useState, useRef } from "react";
+import "./App.css";
 
-function app() {
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000")
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log("error");
-      });
-  });
+function App() {
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioUrl, setAudioUrl] = useState("");
+  const mediaRecorderRef = useRef(null);
+  const chunksRef = useRef([]);
+  const streamRef = useRef(null);
+
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    chunksRef.current = [];
+    streamRef.current = stream;
+
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorderRef.current = mediaRecorder;
+
+    mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        chunksRef.current.push(event.data);
+      }
+    };
+
+    mediaRecorder.onstop = () => {
+      const audioBlob = new Blob(chunksRef.current, { type: "audio/webm" });
+      const url = URL.createObjectURL(audioBlob);
+      setAudioUrl(url);
+    };
+
+    mediaRecorder.start();
+    setIsRecording(true);
+  };
+  const stopRecording = () => {
+    mediaRecorderRef.current.stop();
+    streamRef.current.getTracks().forEach((track) => track.stop());
+    setIsRecording(false);
+  };
 
   return (
-    <div>
-      <h1>AI MOM App</h1>
+    <div className="App">
+      <h2>AI MOM App</h2>
+      <p>Microphone recording</p>
+      {!isRecording ? (
+        <button onClick={startRecording}>Start Recording</button>
+      ) : (
+        <button onClick={stopRecording}> Stop Recording</button>
+      )}
+
+      {audioUrl && (
+        <div className="audio-Box">
+          <p>Recorded audio</p>
+          <audio controls src={audioUrl}></audio>
+        </div>
+      )}
     </div>
   );
 }
 
-export default app;
+export default App;
